71549350bf346ce7ace09c9ea3669831fda631ef
Roy Munialo Wati <munialo.roy@ekenya.co.ke>
2019-10-14 12:49:51 +0300

    origin master

--- src/main/java/chainbox/io/esbformatter/dao/iso8583/IsoConfigurationTemplate.java
+++ src/main/java/chainbox/io/esbformatter/dao/iso8583/IsoConfigurationTemplate.java
@@ -12,7 +12,6 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class IsoConfigurationTemplate {
-
     private String processingCode;
     private String currencyCode;
     private String mtiRequest;
